d7e9def6511e1313c804dc8b81414c3a09397752
“muddyrain” <qiu3291002845@gmail.com>
2023-03-26 13:43:08 +0800

    feat: 81 孔明灯特效

--- flyLight/src/main/index.js
+++ flyLight/src/main/index.js
@@ -4,6 +4,7 @@ import vertexShader from "../shader/flylight/vertex.glsl";
 import fragmentShader from "../shader/flylight/fragment.glsl";
 import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import gsap from "gsap";
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
@@ -13,7 +14,7 @@ const camera = new THREE.PerspectiveCamera(
   300
 );
 // 设置相机位置
-camera.position.set(0, 1, 2);
+camera.position.set(0, -5, 10);
 scene.add(camera);
 
 // 创建环境纹理
@@ -30,12 +31,34 @@ gltfLoader.load("./assets/model/flyLight.glb", (gltf) => {
   scene.add(gltf.scene);
   lightBox = gltf.scene.children[0];
   lightBox.material = shaderMaterial;
+
+  for (let i = 0; i < 150; i++) {
+    let flyLight = gltf.scene.clone(true);
+    let x = (Math.random() - 0.5) * 300;
+    let z = (Math.random() - 0.5) * 300;
+    let y = Math.random() * 60 + 25;
+    flyLight.position.set(x, y, z);
+    gsap.to(flyLight.rotation, {
+      y: 2 * Math.PI,
+      duration: 10 + Math.random() * 30,
+      yoyo: true,
+      repeat: -1,
+    });
+    gsap.to(flyLight.position, {
+      x: "+=" + Math.random() * 5,
+      y: "+=" + Math.random() * 20,
+      duration: 5 + Math.random() * 15,
+      yoyo: true,
+      repeat: -1,
+    });
+    scene.add(flyLight);
+  }
 });
 const shaderMaterial = new THREE.ShaderMaterial({
   vertexShader,
   fragmentShader,
   side: THREE.DoubleSide,
-  transparent: true,
+  // transparent: true,
 });
 
 // 创建渲染函数
@@ -50,6 +73,10 @@ document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
 // 设置控制器的阻尼，让控制器更真实
 controls.enableDamping = true;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 0.5;
+controls.maxPolarAngle = Math.PI;
+controls.minPolarAngle = Math.PI / 4;
 // 添加坐标轴辅助器
 // const axesHelper = new THREE.AxesHelper(5);
 // scene.add(axesHelper);
--- flyLight/src/shader/flylight/fragment.glsl
+++ flyLight/src/shader/flylight/fragment.glsl
@@ -5,5 +5,10 @@ void main() {
     vec4 redColor = vec4(1.0, 0.0, 0.0, 1.0);
     vec4 yellowColor = vec4(1.0, 1.0, 0.0, 1.0);
     vec4 mixColor = mix(yellowColor, redColor, gPosition.y / 3.0);
-    gl_FragColor = vec4(mixColor.rgb, 1.0);
+    if(gl_FrontFacing) {
+        gl_FragColor = vec4(mixColor.xyz - (vPosition.y - 20.0) / 80.0 - 0.1, 1.0);
+    } else {
+        gl_FragColor = vec4(mixColor.xyz, 1.0);
+    }
+
 }
