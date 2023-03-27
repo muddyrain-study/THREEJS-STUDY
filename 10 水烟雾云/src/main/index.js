a53e560f74ad27107ba70fd9f7f0cdccb08eaec7
“muddyrain” <qiu3291002845@gmail.com>
2023-03-27 10:41:44 +0800

    feat: 82 shaderMaterial

---
+++ 10 水烟雾云/src/main/index.js
@@ -0,0 +1,73 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import vertexShader from "../shader/water/vertexShader.glsl";
+import fragmentShader from "../shader/water/fragmentShader.glsl";
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  300
+);
+// 设置相机位置
+camera.position.set(0, 5, 10);
+scene.add(camera);
+
+const shaderMaterial = new THREE.ShaderMaterial({
+  vertexShader,
+  fragmentShader,
+});
+
+const plane = new THREE.Mesh(
+  new THREE.PlaneGeometry(1, 1, 512, 512),
+  shaderMaterial
+);
+plane.rotation.x = -Math.PI / 2;
+scene.add(plane);
+
+// 创建渲染函数
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// 创建轨道控制器
+const controls = new OrbitControls(camera, renderer.domElement);
+// 设置控制器的阻尼，让控制器更真实
+controls.enableDamping = true;
+
+// 添加坐标轴辅助器
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+// 设置时钟
+const clock = new THREE.Clock();
+function render() {
+  let time = clock.getElapsedTime();
+  renderer.render(scene, camera);
+  controls.update();
+  window.requestAnimationFrame(render);
+}
+
+render();
+
+// 监听画面变化，更新渲染画面
+window.addEventListener("resize", () => {
+  // 更新相机的 比例
+  camera.aspect = window.innerWidth / window.innerHeight;
+  // 更新摄像机投影矩阵
+  camera.updateProjectionMatrix();
+  // 更新渲染器
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  // 设置渲染器的像素比
+  renderer.setPixelRatio(window.devicePixelRatio);
+});
+
+window.addEventListener("dblclick", () => {
+  const fullScreenElement = document.fullscreenElement;
+  if (!fullScreenElement) {
+    // 双击控制屏幕进入全屏，退出全屏
+    renderer.domElement.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+});
---
+++ 10 水烟雾云/src/shader/flylight/fragment.glsl
@@ -0,0 +1,14 @@
+precision lowp float;
+varying vec4 vPosition;
+varying vec4 gPosition;
+void main() {
+    vec4 redColor = vec4(1.0, 0.0, 0.0, 1.0);
+    vec4 yellowColor = vec4(1.0, 1.0, 0.0, 1.0);
+    vec4 mixColor = mix(yellowColor, redColor, gPosition.y / 3.0);
+    if(gl_FrontFacing) {
+        gl_FragColor = vec4(mixColor.xyz - (vPosition.y - 20.0) / 80.0 - 0.1, 1.0);
+    } else {
+        gl_FragColor = vec4(mixColor.xyz, 1.0);
+    }
+
+}
