b2a15bfce86c3d2ddaec35ffbe24803462534abf
“muddyrain” <qiu3291002845@gmail.com>
2023-03-27 16:38:31 +0800

    feat: 90 点着色器设置原理

--- 11  shader 粒子效果/src/shader/basic/vertexShader.glsl
+++ 11  shader 粒子效果/src/shader/basic/vertexShader.glsl
@@ -1,8 +1,9 @@
+varying vec2 vUv;
 void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
     gl_Position = projectionMatrix * viewPosition;
-
+    vUv = uv;
     // 设置点大小
     gl_PointSize = 50.0;
 }
